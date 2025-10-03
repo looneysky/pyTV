5577add97d177c50e8fea02de91ffb3cc6d5192c
morfy <63257090+looneysky@users.noreply.github.com>
2025-10-03 17:33:49 +0300

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ from flask import Flask, request, Response, send_from_directory
 
 app = Flask(__name__)
 
-BASE_URL = 'http://cdntv.online/hls/xm1hzalq26'
+BASE_URL = 'https://cdntv.online/hls/ynemik5cl9'
 
 #powered by dezamorfin
 
